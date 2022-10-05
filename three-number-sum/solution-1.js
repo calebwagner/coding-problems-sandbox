91b96cb82c680426a42e957d6b45762cc0c35f05
Caleb <calebwagner5@gmail.com>
2022-10-05 12:58:11 -0500

    three number sum w/ js

--- three-number-sum/solution-1.js
+++ three-number-sum/solution-1.js
@@ -2,9 +2,21 @@ array = [12, 3, 1, 2, -6, 5, -8, 6]
 targetSum = 0;
 
 function threeNumberSum(array, targetSum) {
-    if (array.length == 0) return
-
-    for (let i = 0; i < array.length; i++) {
-        let firstNum = array[i]
+    array.sort((a, b) => a - b); // sort the array desc to asc
+    let triplets = [];
+    // loop thru the array and stop when i is the 3rd to last index in the array
+    for (let i = 0; i < array.length - 2; i++) {
+        // i = current value of index
+        let left = i + 1; // 1 position left of i (left value)
+        let right = array.length - 1; // last value
+        while (left < right) {
+            const currentSum = array[i] + array[left] + array[right];
+            if (currentSum === targetSum) {
+                triplets.push([array[i], array[left++], array[right--]]);
+            }
+            if (currentSum < targetSum) left += 1;
+            if (currentSum > targetSum) right -= 1;
+        }
     }
+    return triplets;
 }
