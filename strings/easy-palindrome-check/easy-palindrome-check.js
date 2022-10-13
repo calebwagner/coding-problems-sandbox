e8e6ba3fe214971b00bbb038ba9e7e50ee3d068b
Caleb <calebwagner5@gmail.com>
2022-10-13 08:57:04 -0500

    solution 1 to palindrome problem

---
+++ strings/easy-palindrome-check/easy-palindrome-check.js
@@ -0,0 +1,15 @@
+/**
+ * 1) write a function that takes in a non-empty string that returns a boolean representing whether the string is a palindrome
+ */
+
+function isPalindrome(string) {
+    if (!string && typeof string != "string") return
+
+    let reversedString = string.split("").reverse().join("")
+
+    if (reversedString === string) {
+        return true
+    }
+
+    return false
+}
