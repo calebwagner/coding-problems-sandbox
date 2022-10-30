85ddb61bd3389a456aa6de3889cc2e40625e7fbb
Caleb <calebwagner5@gmail.com>
2022-10-30 08:18:51 -0500

    working on solution for line through points

---
+++ hard/line-through-points/solution-1.js
@@ -0,0 +1,14 @@
+function lineThoughPoints(points) {
+    let lengthOfPoints = points.length;
+    if (lengthOfPoints <= 2) return lengthOfPoints
+
+    var res = 2;
+
+    for (let i = 0; i < lengthOfPoints - 1; i++) {
+        let m = {
+            _: 0
+        };
+        dup = 1;
+
+    }
+}
