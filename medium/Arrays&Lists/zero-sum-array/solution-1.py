f74465bebf3506f2acdbcabb7b04a53b8b10b6b1
Caleb <calebwagner5@gmail.com>
2022-12-23 08:58:09 -0600

    zero sum array

--- medium/Arrays&Lists/zero-sum-array/solution-1.py
+++ medium/Arrays&Lists/zero-sum-array/solution-1.py
@@ -1,4 +1,7 @@
 # O(n) time | O(n) space
+
+nums = [-5, -5, 2, 3, -2]
+
 def zeroSumSubarray(nums):
     sums = set([0])
     currentSum = 0
@@ -9,3 +12,17 @@ def zeroSumSubarray(nums):
         sums.add(currentSum)
         
     return False
+
+zeroSumSubarray(nums)
+
+
+# def zeroSumSubarray(nums):
+#     sums = set([0])
+#     currentSum = 0
+#     for num in nums:
+#         currentSum += num
+#         if currentSum in sums:
+#             print([nums[0+1], num])
+#         sums.add(currentSum)
+
+#     return False
