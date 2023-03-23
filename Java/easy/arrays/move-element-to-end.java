f9badb6b86750ea2def13b9c3f7694561e92d00f
Caleb <calebwagner5@gmail.com>
2023-03-23 14:26:57 -0500

    move element to end - java solution

---
+++ Java/easy/arrays/move-element-to-end.java
@@ -0,0 +1,20 @@
+class Program {
+  public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+    int i = 0;
+    int j = array.size() - 1;
+
+    while (i < j) {
+      while (i < j && array.get(j) == toMove) j--;
+      if (array.get(i) == toMove) swap(i, j, array);
+      i++;
+    }
+    return array;
+  }
+
+
+public static void swap(int i, int j, List<Integer> array) {
+  int temp = array.get(j);
+  array.set(j, array.get(i));
+  array.set(i, temp);
+ }
+}
