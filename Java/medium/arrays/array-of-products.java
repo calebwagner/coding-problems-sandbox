dcb9cbea2372e1f70564d6fa21044d3e7bc8f554
Caleb <calebwagner5@gmail.com>
2023-03-28 09:22:17 -0500

    array of products - java solution

---
+++ Java/medium/arrays/array-of-products.java
@@ -0,0 +1,16 @@
+class Program {
+    public int[] arrayOfProducts(int[] array) {
+      int[] products = new int[array.length];
+  
+      for (int i = 0; i < array.length; i++) {
+        int runningProduct = 1;
+        for (int j = 0; j < array.length; j++) {
+          if (i != j) {
+            runningProduct *= array[j];
+          }
+          products[i] = runningProduct;
+        }
+      }
+      return products;
+    }
+  }
