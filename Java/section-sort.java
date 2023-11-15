4b1991007ef97bb0c73a655c432bdb356731205f
Caleb <calebwagner5@gmail.com>
2023-11-15 06:56:41 -0600

    section sort java

---
+++ Java/section-sort.java
@@ -0,0 +1,20 @@
+class Program {
+    public static int[] selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    swap(array, i, j);
+                }
+            }
+        }
+
+        return array;
+    }
+
+    private static void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
